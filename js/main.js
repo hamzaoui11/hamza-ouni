a20bb1fafb586ccf8e0309b0d3832d7657ff8b11
hamzaoui11 <42379981+hamzaoui11@users.noreply.github.com>
2018-09-01 15:48:14 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+function  afficher () {
+    document.getElementById("p1").style.visibility = "visible" ;
+    
+}
+function  masquer () {
+    document.getElementById("p1").style.visibility = "hidden" ;
+    
+}
+function  afficher1 () {
+    document.getElementById("p2").style.visibility = "visible" ;
+    
+}
+function  masquer1 () {
+    document.getElementById("p2").style.visibility = "hidden" ;
+    
+}
+function  afficher2 () {
+    document.getElementById("p3").style.visibility = "visible" ;
+    
+}
+function  masquer2 () {
+    document.getElementById("p3").style.visibility = "hidden" ;
+    
+}
+function  afficher3 () {
+    document.getElementById("p4").style.visibility = "visible" ;
+    
+}
+function  masquer3 () {
+    document.getElementById("p4").style.visibility = "hidden" ;
+    
+}function  afficher4 () {
+    document.getElementById("p5").style.visibility = "visible" ;
+    
+}
+function  masquer4 () {
+    document.getElementById("p5").style.visibility = "hidden" ;
+    
+}function  afficher5 () {
+    document.getElementById("p6").style.visibility = "visible" ;
+    
+}
+function  masquer5 () {
+    document.getElementById("p6").style.visibility = "hidden" ;
+    
+}
